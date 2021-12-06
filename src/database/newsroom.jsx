00fdc69993f3ae8f2642b4fada4d591b6af015b8
Yuvraj LM <yuvraj.singh@likeminds.community>
2021-12-07 02:21:18 +0530

    deploy test

--- src/database/newsroom.jsx
+++ src/database/newsroom.jsx
@@ -13,7 +13,7 @@ const articles = [
         tags: ["Business", "Design Thinking"],
         link: "simple-easy-and-quick-dont-make-me-think",
         meta: "This is the meta write anything here that is useful"
-        // logo: "https://media-exp1.licdn.com/dms/image/C5112AQGwVDA9axrryA/article-cover_image-shrink_600_2000/0/1574256672318?e=1643241600&v=beta&t=aB6rgbWaw5wAlZ3LAKs_9SRsMdrtv2iMTWywYFEzrgo",
+        // logo: "https://media-exp1.licdn.com/dms/image/C5112AQGwVDA9afxrryA/article-cover_image-shrink_600_2000/0/1574256672318?e=1643241600&v=beta&t=aB6rgbWaw5wAlZ3LAKs_9SRsMdrtv2iMTWywYFEzrgo",
     },
 ]
 
